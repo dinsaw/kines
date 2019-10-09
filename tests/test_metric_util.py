044aa316213e3620440e7ec5d1384f7d071f91ae
Dinesh Sawant <dineshsawant300@gmail.com>
2019-10-09 16:40:31 +0530

    Add metric util test

---
+++ tests/test_metric_util.py
@@ -0,0 +1,15 @@
+from kines import metric_util
+
+TEST_PERIOD = 5
+TEST_STREAM_NAME = 'test_stream_name'
+TEST_METRIC_NAME = 'test_metric'
+METRIC_ID = 'metric_id'
+
+
+def test_get_metric_data_query():
+    query = metric_util.get_metric_data_query(TEST_STREAM_NAME, TEST_METRIC_NAME, METRIC_ID, TEST_PERIOD)
+    assert query['Id'] == METRIC_ID
+    assert query['MetricStat']['Metric']['MetricName'] == TEST_METRIC_NAME
+    assert query['MetricStat']['Metric']['Dimensions'][0]['Value'] == TEST_STREAM_NAME
+    assert query['MetricStat']['Stat'] == 'Sum'
+    assert query['MetricStat']['Period'] == TEST_PERIOD
